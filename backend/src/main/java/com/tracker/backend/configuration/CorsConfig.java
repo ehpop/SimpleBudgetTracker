26f64caf306e3938ce22b1e30e4c4cbcc48acde0
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2023-11-22 23:41:46 +0100

    Added simple cores configuration

---
+++ backend/src/main/java/com/tracker/backend/configuration/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.tracker.backend.configuration;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Value("${backend.cors.allowed-origins:http://localhost:3000}")
+  private List<String> allowedOrigins;
+
+  @Bean
+  public CorsFilter corsFilter() {
+    var source = new UrlBasedCorsConfigurationSource();
+    var config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(allowedOrigins);
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    config.setAllowedHeaders(List.of("*"));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
+
+}
