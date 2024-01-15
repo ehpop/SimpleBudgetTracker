7dada1d55c0958107034b6987b8f1f06bb53c6fe
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-01-15 23:41:22 +0100

    removed unused files, cleaned up code

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,6 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import {Link} from "react-router-dom";
 
